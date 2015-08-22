0a42cdb4c389ec25fc92efe31e0659682593f9e6
andrewsauder <andrewsauder@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-22 14:31:39 +0000

    fixed version number in main geoseo.php file
    git-svn-id: https://plugins.svn.wordpress.org/local-geo-search/trunk@1228066 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- geoseo.php
+++ geoseo.php
@@ -3,7 +3,7 @@
 * Plugin Name: Local Geo Search
 * Plugin URI: https://www.localgeosearch.com
 * Description: Local GEO Search creates hundreds of location specific pages on your site to target your services in your market.
-* Version: 1.0.0
+* Version: 1.0.1
 * Author: Elite Impressions, LLC
 * Author URI: http://www.localgeosearch.com
 **/
